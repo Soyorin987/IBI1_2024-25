c754bb8e920f68d1f555af245bda9d4d53feb4f4
Soyorin987 <jingyi.24@intl.zju.edu.cn>
2025-03-11 09:41:18 +0100

    MoreCommentForTriangle.py

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -2,10 +2,10 @@
 #T=number of of dots in a triangle
 #if the nth triangle
 #Tn=n*(n+1)/2
-#n range from 1 to 10
-#print T1 to T10
 def numbers_of_dots(n):
     return n*(n+1)/2
+#n range from 1 to 11
+#print T1 to T10
 for n in range(1,11):
     Tn = numbers_of_dots(n)
     print("T"+str(n)+"=",Tn)
