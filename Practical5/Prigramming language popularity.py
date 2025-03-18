b2675b08edddc19e9a91a67e3d937e40153a3eba
Soyorin987 <jingyi.24@intl.zju.edu.cn>
2025-03-18 08:49:13 +0100

    PrintThePercentage

--- Practical5/Prigramming language popularity.py
+++ Practical5/Prigramming language popularity.py
@@ -1,6 +1,6 @@
 my_dict={'JavaScript':62.3, 'HTML':52.9, 'Python':51, 'SQL':51, 'TypeScript':38.5}
 # Create a dictionary with the given data
-print(my_dict)
+
 import numpy as np
 import matplotlib.pyplot as plt
 # Import the matplotlib library
@@ -14,4 +14,12 @@ plt.title('Programming language popularity')
 plt.xlabel('Languages')
 plt.ylabel('Users(percentage)')
 # Add the labels
-plt.show()
+
+Choice=input("Enter a programming language from the list (JavaScript, HTML, Python, SQL, TypeScript): ")
+# Ask the user to enter a programming language from the list
+if Choice in Language:
+    print("The percentage of users who use",Choice,"is",my_dict[Choice],"%")
+# If the user's choice is in the list, print the percentage of users who use that language
+else:
+    print("The programming language is not in the list")
+# If the user's choice is not in the list, tell the user that it is not in the list
