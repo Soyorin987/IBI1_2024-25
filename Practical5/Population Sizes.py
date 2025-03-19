e4ba5e6ce50f42ea06197df740a21f2c2c0d1a1a
Soyorin987 <jingyi.24@intl.zju.edu.cn>
2025-03-19 07:40:21 +0100

    AddSorting

--- Practical5/Population Sizes.py
+++ Practical5/Population Sizes.py
@@ -1,8 +1,10 @@
 uk_countries=[57.11,3.13,1.91,5.45]
+uk_name=['England','Wales','Northern Ireland','Scotland']
 Zhejiang_neighbouring_provinces_in_China=[65.77,41.88,45.28,61.27,85.15]
+Zhejiang_neighbouring_provinces_name=['Zhejiang','Fujian','Jiangxi','Anhui','Jiangsu']
 # Make two lists using the given data
-print("The population sizes of the UK countries are",uk_countries)
-print("The population sizes of the neighbouring provinces of Zhejiang in China are",Zhejiang_neighbouring_provinces_in_China)
+print("The population sizes of the UK countries in ascending order:",sorted(uk_countries))
+print("The population sizes of the neighbouring provinces of Zhejiang in China in ascending order:",sorted(Zhejiang_neighbouring_provinces_in_China))
 # Print the two lists
 
 #Firslty, draw the pie chart of the distrbution of population sizes in UK countries
