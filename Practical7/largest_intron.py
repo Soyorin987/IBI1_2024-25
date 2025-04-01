b26d7ac94a34ac69e86dcb0026b7c988e2c02690
Soyorin987 <jingyi.24@intl.zju.edu.cn>
2025-04-01 08:28:58 +0100

    largestIntro

---
+++ Practical7/largest_intron.py
@@ -0,0 +1,21 @@
+seq='ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
+splice_donor='GT'
+splice_acceptor='AG'
+#set the splice donor and splice acceptor
+largest_intron_length=0
+#initialize the largest intron length to 0
+largest_intron=''
+
+#start to search for the largest intron
+for i in range(len(seq)):
+    if seq[i:i+2]==splice_donor:
+        for j in range(i+2, len(seq)):
+            if seq[j:j+2]==splice_acceptor:
+                d=seq[i:j+2]
+                
+        if len(d)>largest_intron_length:
+            largest_intron_length=len(d)
+
+# Print the largest intron and its length
+print("Largest intron:", largest_intron)
+print("Length of the largest intron:", largest_intron_length)
