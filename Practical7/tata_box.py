6d553684145ecbbab3d73aab5b787d8cbf35a339
Soyorin987 <jingyi.24@intl.zju.edu.cn>
2025-04-01 16:27:09 +0100

    Tata

--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -1,29 +1,39 @@
+import re
+
+# set the input and output file paths
 input_file_path = r'C:\cygwin64\home\Stat9\IBI1_2024-2025\IBI1_2024-25\Practical7\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
 output_file_path = r'C:\cygwin64\home\Stat9\IBI1_2024-2025\IBI1_2024-25\Practical7\tata_genes.fa'
+
+# set the maximum line length for output, in case the line is too long
 MAX_LINE_LENGTH = 30
-#set the maximum line length to 30
+
+# define the TATA box pattern
+TATA_BOX_PATTERN = "TATA[AT]A[AT]"
+
 with open(input_file_path, 'r') as infile, open(output_file_path, 'w') as outfile:
     current_header = None
     current_sequence = []
 
     for line in infile:
         line = line.strip()
-        if line.startswith(">"):  # detect header line
-            if current_header:  # if not the first gene
-                outfile.write(f"{current_header}\n")
-                for i in range(0, len(''.join(current_sequence)), MAX_LINE_LENGTH):
-                    outfile.write(f"{''.join(current_sequence)[i:i+MAX_LINE_LENGTH]}\n")
-            # select the header, remove the first space and everything after the first space
-            # in this case we get the gene name
-            current_header = line.split(" ")[0]
+        if line.startswith(">"):  # find the start of a new gene
+            if current_header and current_sequence:  # if there is a previous gene, process it
+                full_sequence = ''.join(current_sequence)  # combine sequence lines
+                if re.search(TATA_BOX_PATTERN, full_sequence):  # check for TATA box
+                    outfile.write(f"{current_header}\n")
+                    for i in range(0, len(full_sequence), MAX_LINE_LENGTH):
+                        outfile.write(f"{full_sequence[i:i+MAX_LINE_LENGTH]}\n")
+            # update the current header and reset sequence
+            current_header = line.split(" ")[0]  # take the header line and remove extra info
             current_sequence = []
         else:
             current_sequence.append(line)  # collect sequence lines
+    # process the last gene
+    if current_header and current_sequence:
+        full_sequence = ''.join(current_sequence)
+        if re.search(TATA_BOX_PATTERN, full_sequence):  
+              outfile.write(f"{current_header}\n")
+        for i in range(0, len(full_sequence), MAX_LINE_LENGTH):
+                outfile.write(f"{full_sequence[i:i+MAX_LINE_LENGTH]}\n")
 
-    # write the last gene if it exists
-    if current_header:
-
-        outfile.write(f"{current_header}\n{''.join(current_sequence)}\n")
-        for i in range(0, len(''.join(current_sequence)), MAX_LINE_LENGTH):
-            outfile.write(f"{''.join(current_sequence)[i:i+MAX_LINE_LENGTH]}\n")
-print(f"Formatted genes saved to '{output_file_path}'.")
+print(f"Genes containing TATA box saved to '{output_file_path}'.")
