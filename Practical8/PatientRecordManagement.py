f9add2891c317eca51faf9deec79fe52fc245842
Soyorin987 <jingyi.24@intl.zju.edu.cn>
2025-04-08 09:11:55 +0100

    return

--- Practical8/PatientRecordManagement.py
+++ Practical8/PatientRecordManagement.py
@@ -6,9 +6,12 @@ class Patient:
         self.age = age
         self.date_of_latest_admission = date_of_latest_admission
         self.medical_history = medical_history
-
+        return
+    
     def display(self):
         print("Name: " + self.name + ", Age: " + str(self.age) + ", Date of Latest Admission: " + self.date_of_latest_admission + ", Medical History: " + self.medical_history)
+        return
+    
 #an example of using the class    
 patient1 = Patient("John Doe", 45, "2025-04-01", "Diagnosed with hypertension.")
 patient1.display()
